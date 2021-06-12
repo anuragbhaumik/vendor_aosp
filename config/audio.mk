84a34f248b8780dd6c50ac71ea386bcaf112cca0
Introdructor <yashkachave7@gmail.com>
2021-06-12 07:22:56 +0000

    vendor:update default sounds

--- config/audio.mk
+++ config/audio.mk
@@ -69,6 +69,6 @@ PRODUCT_COPY_FILES += \
 
 # Change default sounds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=The_big_adventure.ogg \
-    ro.config.notification_sound=Popcorn.ogg \
+    ro.config.ringtone=Hotline.ogg \
+    ro.config.notification_sound=Beginning.ogg \
     ro.config.alarm_alert=Bright_morning.ogg
